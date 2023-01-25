41be8cb864550c438081f7f9a743b0175ddea8d3
«VladimirD» <fotodrom@rambler.ru>
2023-01-25 21:06:41 +0300

    example 005_if else

---
+++ EXAMPLES/examples005_if else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "вован")
+{
+    Console.WriteLine(" Ура!!!! это Вован");
+}
+else {
+    Console. Write(" Привет ");
+    Console.WriteLine(username);
+}
